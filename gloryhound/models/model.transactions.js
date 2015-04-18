1ca0fe00b4293c7105767fff7ce9e3d73bb5ecec
Ethan Presberg <ethan@colosa.com>
2015-04-18 16:38:50 -0400

    Fixed transactions class

--- gloryhound/models/model.transactions.js
+++ gloryhound/models/model.transactions.js
@@ -1,25 +1,25 @@
 TransactionsModel = new Mongo.Collection('transactions');
 
 Transactions = {
-    item: Items,
+    item: '',
     amount: '',
     date: '',
     user: '',
 
     create: function(obj){
-        if (typeof obj.item === 'undefined') return 'Collection needs a valid item property';
+        if (typeof obj.item === 'undefined') return 'Collection needs a valid item id';
         if (typeof obj.amount === 'undefined') return 'Collection needs a valid amount property';
         if (typeof obj.date === 'undefined') return 'Collection needs a valid date property';
-        if (typeof obj.user === 'undefined') return 'Collection needs a user date property';
+        if (typeof obj.user === 'undefined') return 'Collection needs a user id';
         return Transactions.insert(obj);
     },
-    getItem: function(id){
+    getTransaction: function(id){
         return Transactions.find(id).fetch();
     },
-    getAllItems: function(){
+    getAllTransactions: function(){
         return Transactions.find({}).fetch();
     },
-    updateItem: function(id, obj){
+    updateTransaction: function(id, obj){
         return Transactions.update(
             id,
             obj
