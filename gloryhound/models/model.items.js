a0041bbaa0dfbdeb943248604312ed89b7869e22
Ethan Presberg <ethan@colosa.com>
2015-04-18 14:39:26 -0400

    added shared dir

--- gloryhound/models/model.items.js
+++ gloryhound/models/model.items.js
@@ -1,15 +1,18 @@
 /**
  * Created by ethan on 4/18/15.
  */
-Items = new Mongo.Collection('items');
-Items.helpers({
-    create: function(obj) {
-        this.insert( { item: "card", qty: 15 } )
-    },
-    getAll: function(){
-        return this.item + ' ' + this.qty
-    }
-});
+if (Meteor.isServer) {
+    Items = new Mongo.Collection('items');
+    Items.helpers({
+        create: function(obj) {
+            this.insert( { item: "card", qty: 15 } )
+        },
+        getAll: function(){
+            return this.item + ' ' + this.qty
+        }
+    });
+
+}
 
 Meteor.publish('Items', function(){
     return new Items();
