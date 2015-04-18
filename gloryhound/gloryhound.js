7acf4ad76784ee4193ced7430b64a4715e5cabe8
Ethan Presberg <ethan@colosa.com>
2015-04-18 13:20:08 -0400

    Fixed facebook config issue

--- gloryhound/gloryhound.js
+++ gloryhound/gloryhound.js
@@ -20,14 +20,14 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     //Add facebook creds
-    ServiceConfiguration.configurations.update({
-      service:"facebook"
-    }, {
-      $set: {
-        clientId: "1439955059635933",
-        loginStyle: "popup",
-        secret: "40365ab10e5af7922e10b07f81fc69b0"
-    }
-  });
+    ServiceConfiguration.configurations.remove({
+      service: 'facebook'
+    });
+
+    ServiceConfiguration.configurations.insert({
+      service: 'facebook',
+      appId: '1439955059635933',
+      secret: '40365ab10e5af7922e10b07f81fc69b0'
+    });
   });
 }
