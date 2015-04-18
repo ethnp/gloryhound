d32f29e351874f6d082605d10a170641e89d822e
Ethan Presberg <ethan@colosa.com>
2015-04-18 16:49:36 -0400

    Fixed new goal route and added some properties

--- gloryhound/models/model.items.js
+++ gloryhound/models/model.items.js
@@ -5,9 +5,13 @@ ItemsModel = new Mongo.Collection('items');
 
 Items = {
     name: '',
+    category: '',
+    charity: '',
 
     create: function(obj){
-        if (typeof obj.name === 'undefined') return 'Collection needs a valid name property';
+        if (typeof obj.name === 'undefined') return 'Collection needs a valid name';
+        if (typeof obj.category === 'undefined') return 'Collection needs a valid category';
+        if (typeof obj.charity === 'undefined') return 'Collection needs a valid charity';
         return ItemsModel.insert(obj);
     },
     getItem: function(id){
